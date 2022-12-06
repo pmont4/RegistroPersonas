004d200ca7f1e88f1f8ba71046fba97d9181c3f7
Paulo Monterroso <84726204+GiyuDev@users.noreply.github.com>
2022-12-06 03:14:21 -0600

    Update JSON_Configuration.java

--- RegistroPersonas/src/main/java/json/JSON_Configuration.java
+++ RegistroPersonas/src/main/java/json/JSON_Configuration.java
@@ -6,7 +6,6 @@ import entities.Administrator;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
