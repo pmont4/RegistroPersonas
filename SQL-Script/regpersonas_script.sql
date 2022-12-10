944d91fd6a8df91dfb9ae8ffafb6c9ef4628397b
Paulo Monterroso <84726204+GiyuDev@users.noreply.github.com>
2022-12-09 19:56:21 -0600

    Update regpersonas_script.sql

--- SQL-Script/regpersonas_script.sql
+++ SQL-Script/regpersonas_script.sql
@@ -6,7 +6,7 @@ DROP TABLE administrators;
 DROP TABLE people;
 
 SELECT * FROM administrators;
-SELECT * FROM regpersonas.persons ORDER BY (name);
+SELECT * FROM regpersonas.people ORDER BY (name);
 
 SELECT * FROM paulo_log;
 
@@ -14,3 +14,5 @@ TRUNCATE people;
 TRUNCATE administrators;
 
 DROP TABLE paulo_log;
+
+UPDATE people SET birth_date = '1966-01-02' WHERE id = 7;
